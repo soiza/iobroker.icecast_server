1717b240a8b551374b89e6d1d82166cc206cf7fa
soiza <salzer.lukas@gmail.com>
2021-01-06 15:10:54 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"icecast_server adapter settings": {
+		"en": "Adapter settings for icecast_server",
+		"de": "Adaptereinstellungen für icecast_server",
+		"ru": "Настройки адаптера для icecast_server",
+		"pt": "Configurações do adaptador para icecast_server",
+		"nl": "Adapterinstellingen voor icecast_server",
+		"fr": "Paramètres d'adaptateur pour icecast_server",
+		"it": "Impostazioni dell'adattatore per icecast_server",
+		"es": "Ajustes del adaptador para icecast_server",
+		"pl": "Ustawienia adaptera dla icecast_server",
+		"zh-cn": "icecast_server的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
